48ad926437cdbc7099f6a03b622553093a75cbae
Gnapika-05 <gnapikareddy53@gmail.com>
2025-07-04 17:07:07 +0530

    July 4 Java fundamentals

---
+++ java_fundamentals/src/java_fundamentals/LB_Question_2.java
@@ -0,0 +1,13 @@
+package java_fundamentals;
+
+
+public class LB_Question_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome "+args[0]);
+		
+
+	}
+
+}
---
+++ java_fundamentals/src/java_fundamentals/LB_Question_3.java
@@ -0,0 +1,14 @@
+package java_fundamentals;
+
+
+public class LB_Question_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1=Integer.parseInt(args[0]);
+		int number2=Integer.parseInt(args[1]);
+		System.out.println("The sum of "+number1+" and "+number2+" is "+(number1+number2));
+
+	}
+
+}
